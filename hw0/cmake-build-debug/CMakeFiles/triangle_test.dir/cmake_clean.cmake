20c4ef22e6e38ca9db2b4f713d618a68d920032c
Fan <zhangfan.v@outlook.com>
2019-01-30 01:27:45 -0800

    hw0

---
+++ hw0/cmake-build-debug/CMakeFiles/triangle_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/triangle_test.dir/triangle_test.cpp.o"
+  "triangle_test.pdb"
+  "triangle_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/triangle_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
